963640aa2532e7028e631ed26bd832c8dd2a5a1c
Yakir Ben Hillel <yakir985@gmail.com>
2019-09-07 21:48:05 +0300

    lol

---
+++ build/precache-manifest.a2805e43bdcbb474ac1ffcb91b253196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ae56d0c4eeb27ed1496d7842eb1bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4a7e42786ea2efe8f25",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b4a7e42786ea2efe8f25",
+    "url": "/static/js/2.6abddbb5.chunk.js"
+  },
+  {
+    "revision": "64c9979373e8dfda41d0",
+    "url": "/static/js/main.80a7aa39.chunk.js"
+  },
+  {
+    "revision": "c6954e56879b9bbb8d53",
+    "url": "/static/js/runtime~main.0d53b971.js"
+  }
+]);
